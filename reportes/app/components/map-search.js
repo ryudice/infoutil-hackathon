17e96615e6c40b224268bd2b399655d3fca07ee1
caleb <caleb@debian>
2014-07-25 15:16:13 -0600

    populate_cities

--- reportes/app/components/map-search.js
+++ reportes/app/components/map-search.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-
+import SearchView from 'reportes/views/search-view';
 export default Ember.Component.extend({
 	classNames:['map'],
 	attributeBindings: ['style'],
@@ -8,9 +8,15 @@ export default Ember.Component.extend({
 		var mapOptions = {
           center: new google.maps.LatLng(13.694025, -88.857422),
           zoom: 9,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          disableDefaultUI: true
         };
         var map = new google.maps.Map(this.$()[0],
             mapOptions);
+        var view = SearchView.create({
+         map :map
+        })
+        view.append();
+           
 	}
 });
